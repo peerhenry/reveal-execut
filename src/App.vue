3ba80dc871153f56a37ffb91656927ba1d3fb03a
peer <peer@infi.nl>
2019-04-30 12:27:59 +0200

    add reveal

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,42 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="reveal">
+      <div class="slides">
+        <section>Single Horizontal Slide</section>
+        <section>
+          <section>Vertical Slide 1</section>
+          <section>Vertical Slide 2</section>
+        </section>
+      </div>
     </div>
-    <router-view />
   </div>
 </template>
 
+<script>
+import Reveal from 'reveal.js/js/reveal'
+
+export default {
+  name: 'app',
+  components: {
+    // HelloWorld
+  },
+  mounted() {
+    Reveal.initialize()
+  },
+}
+</script>
+
 <style lang="scss">
+@import '~reveal.js/css/reveal.css';
+@import '~reveal.js/css/theme/white.css';
+
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  /* margin-top: 60px; */
+  height: 100vh;
 }
 </style>
